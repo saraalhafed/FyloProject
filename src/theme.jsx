c69a9f9ad889392d93e8052a388eebae1497e4ff
sara <saraalhafiy89@gmail.com>
2024-07-22 23:58:18 +0200

    first commit

---
+++ src/theme.jsx
@@ -0,0 +1,27 @@
+export const theme = {
+    colors: {
+        header:{
+            bg:'#fff',
+            text:'#040037',
+            link:'#040037',
+        },
+        teams:{
+            bg: '#F9F8FE',
+            text:'#040037',
+            link:{hover:'#76E2CB', link:'#76E2CB'}
+        },
+        signup:{
+            bg:'#575989',
+            text:'#FEFFFF'
+        },
+        footer:{
+            bg:'#07043B',
+            text:'#FEFFFB',
+            link:{hover:'#6E99FF'}
+        },
+        button:{
+            bg:'#2F65F9'
+        }
+    }
+}
+/* we cannot use it inside the global */
